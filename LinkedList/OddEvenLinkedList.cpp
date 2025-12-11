29b88176e812e35a13ef1cf9ac552fef578a647f
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2025-12-11 14:36:03 +0530

    Solved Leetcode 142. Linked List Cycle II (Medium)

--- LinkedList/OddEvenLinkedList.cpp
+++ LinkedList/OddEvenLinkedList.cpp
@@ -8,17 +8,21 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* oddEvenList(ListNode* head) {
-        if (head == NULL || head->next == NULL) return head;
-        ListNode* odd = head;
-        ListNode* even = head->next;
-        ListNode* evenStart = head->next;
-        while (even != NULL && even->next != NULL) {
+    ListNode *oddEvenList(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+            return head;
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *evenStart = head->next;
+        while (even != NULL && even->next != NULL)
+        {
             odd->next = even->next;
             even->next = even->next->next;
-            
+
             odd = odd->next;
             even = even->next;
         }
