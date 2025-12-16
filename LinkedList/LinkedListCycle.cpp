16fc03d8371f4854c238b3559419f9d0aab5e5fe
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2025-12-17 05:08:20 +0530

    Solved Leetcode 143. Reorder List (Medium)!)

--- LinkedList/LinkedListCycle.cpp
+++ LinkedList/LinkedListCycle.cpp
@@ -6,15 +6,19 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    bool hasCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast != NULL && fast -> next != NULL) {
-            slow = slow -> next;
-            fast = fast -> next -> next;
-            if (slow == fast) return true;
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                return true;
         }
         return false;
     }
