2b09c86f3964bc13c9e11fb53285322b3e2dfb38
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-01-02 14:48:06 +0530

    Solved Leetcode 705. Design HashSet (Hard)!

---
+++ Arrays/DesignHashSet.cpp
@@ -0,0 +1,41 @@
+class MyHashSet {
+public:
+    int M;
+    vector<list<int>> buckets;
+    MyHashSet() {
+        M = 15000;
+        buckets.resize(M);
+    }
+    int getIndex (int key) {
+        return key % M;
+    }
+    
+    void add(int key) {
+        int index = getIndex (key);
+        auto itr = find(buckets[index].begin(), buckets[index].end(), key);
+        if (itr == buckets[index].end()) 
+            buckets[index].push_back(key);
+    }
+    
+    void remove(int key) {
+        int index = getIndex (key);
+        auto itr = find(buckets[index].begin(), buckets[index].end(), key);
+        if (itr != buckets[index].end())
+            buckets[index].erase(itr);
+    }
+    
+    bool contains(int key) {
+        int index = getIndex (key);
+        auto itr = find(buckets[index].begin(), buckets[index].end(), key);
+        if (itr == buckets[index].end()) return false;
+        return true;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
