ad67f27fc15fa0f2880b8e62649bf5b76fd3d7bf
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-01-04 18:49:44 +0530

    Solved Leetcode 3798. Largest Even Number (Easy)!

---
+++ Strings/LargestEvenNumber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string largestEven(string s) {
+        while (!s.empty()) {
+            int lastDigit = s.back() - '0';
+            if (lastDigit % 2 == 0)
+                return s;
+            s.pop_back();
+        }
+        return "";
+    }
+};
