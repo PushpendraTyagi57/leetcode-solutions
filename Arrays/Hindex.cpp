a72c1e88f89d0ee85f89e812bc8c0472ba7bf992
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-01-16 05:51:18 +0530

    Solved Leetcode 274. H-Index (Medium)!

---
+++ Arrays/Hindex.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int result = 0;
+        vector<int> count(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            if (citations[i] < n) count[citations[i]]++;
+            else count[n]++;
+        }
+        for (int i = n; i >= 0; i--) {
+            result += count[i];
+            if (result >= i) return i;
+        }
+        return 0;
+    }
+};
