fd171352909a588f6b5b5743c56de2cca8c64c23
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-02-03 01:22:19 +0530

    Solved Leetcode 121. Best Time to Buy and Sell Stock (Easy)!

---
+++ DynamicProgramming/BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buyPrice = prices[0];
+        int maxProfit = 0;
+        int n = prices.size();
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < buyPrice) buyPrice = prices[i];
+            else {
+                int currProfit = prices[i] - buyPrice;
+                maxProfit = max(maxProfit, currProfit);
+            }
+        }
+        return maxProfit;
+    }
+};
