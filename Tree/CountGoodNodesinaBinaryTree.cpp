a9fc18bc3b00114d3160ab43a5728346521eb06f
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-02-15 04:05:24 +0530

    Solved Leetcode 1448. Count Good in Binary Tree (Medium)!

---
+++ Tree/CountGoodNodesinaBinaryTree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void countGoodNodes(TreeNode* root, int& count, int maxVal) {
+        if (root == NULL)
+            return;
+        if (root->val >= maxVal) {
+            count++;
+            maxVal = root->val;
+        }
+        countGoodNodes(root->left, count, maxVal);
+        countGoodNodes(root->right, count, maxVal);
+    }
+    int goodNodes(TreeNode* root) {
+        int count = 0;
+        if (root == NULL)
+            return count;
+        countGoodNodes(root, count, root->val);
+        return count;
+    }
+};
