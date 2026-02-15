da3aa47687952300b303b1d24cee1df1715066fb
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-02-15 08:56:08 +0530

    Solved Leetcode 67. Add Binary (Easy)!

---
+++ Math/AddBinary.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int m = a.size() - 1;
+        int n = b.size() - 1;
+        string result = "";
+        int sum = 0;
+        int carry = 0;
+        while (m >= 0 || n >= 0) {
+            sum = carry;
+            if (m >= 0) {
+                sum += a[m] - '0';
+                m--;
+            }
+            if (n >= 0) {
+                sum += b[n] - '0';
+                n--;
+            }
+            result.push_back((sum % 2 == 0) ? '0' : '1');
+            carry = sum / 2;
+        }
+        if (carry)
+            result.push_back('1');
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
