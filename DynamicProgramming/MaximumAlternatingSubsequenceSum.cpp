337cdacc822cf646f1a675a1a88b90537ddbffa1
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-03-09 21:11:21 +0530

    Solved Leetcode 1911. Maximum Alternating Subsequence Sum (Medium)!

---
+++ DynamicProgramming/MaximumAlternatingSubsequenceSum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int n;
+    vector<vector<long long>> dp;
+    long long solve(int idx, vector<int>& nums, bool flag) {
+        if (idx >= n) return 0;
+        if (dp[idx][flag] != -1) return dp[idx][flag];
+        long long skip = solve(idx + 1, nums, flag);
+        long long val = nums[idx];
+        if (flag == false)
+            val = -val;
+        long long take = solve(idx + 1, nums, !flag) + val;
+        return dp[idx][flag] = max(skip, take);
+
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        n = nums.size();
+        dp.assign(n, vector<long long>(2, -1));
+        return solve(0, nums, true);
+    }
+};
+
+int main() {
+    int k;
+    cin >> k;
+    vector<int> nums(k);
+    for (int i = 0; i < k; i++)
+        cin >> nums[i];
+    Solution obj;
+    long long ans = obj.maxAlternatingSum(nums);
+    cout << ans << endl;
+    return 0;
+}
