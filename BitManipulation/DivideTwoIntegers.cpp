8fec7b88c3877a55a6fa418b9a1436f511c82313
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-03-30 19:17:52 +0530

    Solved Leetcode 29. Divide Two Integers (Medium)!

---
+++ BitManipulation/DivideTwoIntegers.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == divisor) return 1;
+        bool sign = true;
+        if (dividend >= 0 && divisor < 0) sign = false;
+        if (dividend < 0 && divisor >= 0) sign = false;
+        long n = abs((long)(dividend));
+        long d = abs((long)(divisor));
+        long quotient = 0;
+        while (n >= d) {
+            int count = 0;
+            while (n >= (d << (count + 1)))
+                count += 1;
+            quotient += 1 << count;
+            n -= (d << count);
+        }
+        if (quotient == (1 << 31) && sign) return INT_MAX;
+        if (quotient == (1 << 31) && !sign) return INT_MIN;
+        return sign ? quotient : -quotient;
+    }
+};
+
+int main() {
+    int dividend, divisor;
+    cin >> dividend >> divisor;
+    Solution obj;
+    cout << obj.divide(dividend, divisor) << endl;
+    return 0;
+}
