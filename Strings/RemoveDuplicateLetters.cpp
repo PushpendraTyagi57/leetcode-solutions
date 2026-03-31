eabc91f5108e6aca13d4773fbc5920729a08b2b6
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-03-31 20:46:04 +0530

    Solved Leetcode 316. Remove Duplicate Letters (Medium)!

---
+++ Strings/RemoveDuplicateLetters.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+        vector<bool> taken(26, false);
+        vector<int> lastIndex(26);
+        for (int i = 0; i < n; i++)
+            lastIndex[s[i] - 'a'] = i;
+        string result;
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (taken[ch - 'a']) continue;
+            while (result.size() > 0 && result.back() > ch && lastIndex[result.back() - 'a'] > i) {
+                taken[result.back() - 'a'] = false;
+                result.pop_back();
+            }
+            result.push_back(ch);
+            taken[ch - 'a'] = true;
+        }
+        return result;
+    }
+};
+
+int main() {
+    string s;
+    cin >> s;
+    Solution obj;
+    cout << obj.removeDuplicateLetters(s) << endl;
+    return 0;
+}
