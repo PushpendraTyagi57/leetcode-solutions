8f66aeeaf14bc0a1216c81e9445abcf9d679f6cf
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-04-08 08:12:24 +0530

    Solved Leetcode 100. Same Tree (Easy)!

---
+++ Tree/SameTree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL) return true;
+        if (p == NULL || q == NULL) return false;
+        queue<TreeNode*> que1, que2;
+        que1.push(p);
+        que2.push(q);
+        while (!que1.empty() && !que2.empty()) {
+            TreeNode* node1 = que1.front();
+            que1.pop();
+            TreeNode* node2 = que2.front();
+            que2.pop();
+            if (node1->val != node2->val) return false;
+            if (node1->left != NULL && node2->left != NULL) {
+                que1.push(node1->left);
+                que2.push(node2->left);
+            }
+            else if (node1->left != NULL || node2->left != NULL) return false;
+            if (node1->right != NULL && node2->right != NULL) {
+                que1.push(node1->right);
+                que2.push(node2->right);
+            }
+            else if (node1->right != NULL || node2->right != NULL) return false;
+        }
+        return true;
+    }
+};
