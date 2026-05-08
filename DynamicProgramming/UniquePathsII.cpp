4f4545cea4a53e0cb00e32f65a4a18583c9ecb5a
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-05-08 22:20:53 +0530

    Solved Leetcode 63. Unique Paths II (Medium)!

---
+++ DynamicProgramming/UniquePathsII.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n));
+        if (obstacleGrid[0][0] == 1) return 0;
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) continue;
+                int up = 0, left = 0;
+                if (i - 1 >= 0) up = dp[i - 1][j];
+                if (j - 1 >= 0) left = dp[i][j - 1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    vector<vector<int>> obstacleGrid(x, vector<int>(y));
+    for (int i = 0; i < x; i++)
+        for (int j = 0; j < y; j++)
+            cin >> obstacleGrid[i][j];
+    Solution obj;
+    cout << obj.uniquePathsWithObstacles(obstacleGrid) << endl;
+    return 0;
+}
