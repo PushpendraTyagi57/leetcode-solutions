a09e87531a459611ae3876f6709a1c7a9301eb3b
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-06-11 16:08:14 +0530

    Solved Leetcode 117. Populating Next Right Pointers in Each Node II (Medium)!

---
+++ Tree/PopulatingNextRightPointersinEachNodeII.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == NULL) return NULL;
+        queue<Node*> que;
+        que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            while (size--) {
+                Node* temp = que.front();
+                que.pop();
+                if (size != 0) temp->next = que.front();
+                if (temp->left) que.push(temp->left);
+                if (temp->right) que.push(temp->right);
+            }
+        }
+        return root;
+    }
+};
