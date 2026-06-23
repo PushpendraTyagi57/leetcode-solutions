aba8e0ccffed55e46ff6885a27a10576b54a2c75
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-06-23 14:42:26 +0530

    Solved Leetcode 208. Implement Trie (Prefix Tree)!

---
+++ Tries/ImplementTrie(PrefixTree).cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Trie {
+public:
+    struct trieNode {
+        bool isEnd;
+        trieNode* children[26];
+    };
+    
+    trieNode* getNode() {
+        trieNode* newNode = new trieNode();
+        newNode->isEnd = false;
+        for (int i = 0; i < 26; i++)
+            newNode->children[i] = NULL;
+        return newNode;
+    }
+
+    trieNode* root;
+    Trie() {
+        root = getNode();
+    }
+    
+    void insert(string word) {
+        trieNode* crawler = root;
+        for (int i = 0; i < word.size(); i++) {
+            char ch = word[i];
+            int idx = ch - 'a';
+            if (crawler->children[idx] == NULL)
+                crawler->children[idx] = getNode();
+            crawler = crawler->children[idx];
+        }
+        crawler->isEnd = true;
+    }
+    
+    bool search(string word) {
+        trieNode* crawler = root;
+        for (int i = 0; i < word.size(); i++) {
+            char ch = word[i];
+            int idx = ch - 'a';
+            if (crawler->children[idx] == NULL)
+                return false;
+            crawler = crawler->children[idx];
+        }
+        if (crawler != NULL && crawler->isEnd == true) return true;
+        return false;
+    }
+    
+    bool startsWith(string prefix) {
+        trieNode* crawler = root;
+        int i = 0;
+        for (; i < prefix.size(); i++) {
+            char ch = prefix[i];
+            int idx = ch - 'a';
+            if (crawler->children[idx] == NULL)
+                return false;
+            crawler = crawler->children[idx];
+        }
+        if (i == prefix.size()) return true;
+        return false;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
