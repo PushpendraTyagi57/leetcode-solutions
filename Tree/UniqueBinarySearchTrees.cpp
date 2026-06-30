9dceb77f7eb063c6acc6f1a87ac75134ae513abf
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-06-30 19:31:18 +0530

    Solved Leetcode 96. Unique Binary Search Trees (Medium)!

---
+++ Tree/UniqueBinarySearchTrees.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                int leftNodes = j - 1;
+                int rightNodes = i - j;
+                dp[i] += dp[leftNodes] * dp[rightNodes];
+            }
+        }
+        return dp[n];
+
+    }
+};
+
+int main() {
+    Solution obj;
+    int n; cin >> n;
+    cout << obj.numTrees(n) << endl;
+    return 0;
+}
