cdb12d8bf107176b0643b95671db0a28e0c1da1b
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-07-15 09:51:19 +0530

    Solved LeetCode 3658. GCD of Odd and Even Sums (Easy)!

---
+++ Math/GCDofOddandEvenSums.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+int main() {
+    Solution solution;
+    int n;
+    cin >> n;
+    cout << solution.gcdOfOddEvenSums(n) << endl;
+    return 0;
+}
