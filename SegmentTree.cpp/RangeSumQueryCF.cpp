184391185b3115f4c73ea0883a74d64523fb0ecd
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-07-20 12:57:32 +0530

    Solved Codeforces Y. Range sum query!

---
+++ SegmentTree.cpp/RangeSumQueryCF.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int sumQuery(int left, int right, vector<int>& prefixSum) {
+        if (left == 0) return prefixSum[right];
+        return prefixSum[right] - prefixSum[left - 1];
+    }
+};
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> nums(N);
+    for (int i = 0; i < N; i++) cin >> nums[i];
+    vector<vector<int>> queries(Q, vector<int>(2));
+    for (int i = 0; i < Q; i++) {
+        cin >> queries[i][0] >> queries[i][1];
+    }
+    vector<int> prefixSum(N);
+    prefixSum[0] = nums[0];
+    for (int i = 1; i < N; i++) prefixSum[i] = prefixSum[i - 1] + nums[i]; 
+    Solution obj;
+    for (int i = 0; i < Q; i++) {
+        int first = queries[i][0];
+        int second = queries[i][1];
+        cout << obj.sumQuery(first - 1, second - 1, prefixSum) << "\n";
+    }
+    return 0;
+}
