87445d1caae2615f1ba660595be66dfa91ff1b81
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-07-24 08:46:30 +0530

    Solved LeetCode 180. Consecutive Numbers (Medium)!

---
+++ SQL/ConsecutiveNumbers.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT l1.num AS ConsecutiveNums
+FROM Logs l1
+CROSS JOIN Logs l2
+CROSS JOIN Logs l3
+WHERE l1.id - l2.id = 1
+    AND l2.id - l3.id = 1
+    AND l1.num = l2.num
+    AND l2.num = l3.num
+GROUP BY l1.num;
