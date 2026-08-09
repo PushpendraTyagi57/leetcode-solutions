d598a41210ad70b262b895d63169fbe3990ed01b
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-08-10 04:32:55 +0530

    Solved LeetCode 222. Count Complete Tree Nodes (Medium)!

---
+++ Tree/CountCompleteTreeNodes.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class CountCompleteTreeNodes {
+    public int countLeftHeight(TreeNode root) {
+        int height = 0;
+        while (root != null) {
+            height++;
+            root = root.left;
+        }
+        return height;
+    }
+    public int countRightHeight(TreeNode root) {
+        int height = 0;
+        while (root != null) {
+            height++;
+            root = root.right;
+        }
+        return height;
+    }
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        int left = countLeftHeight(root);
+        int right = countRightHeight(root);
+        if (left == right) return (1 << left) - 1;
+        return 1 + countNodes(root.left) + countNodes(root.right); 
+    }
+}
