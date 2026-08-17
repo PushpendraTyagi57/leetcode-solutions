dce75b651616ec1e3fed378b6f347640be7cded8
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-08-17 21:49:24 +0530

    Solved LeetCode 2029. Stone Game IX (Medium)!

---
+++ Math/StoneGameIX.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int c0 = 0, c1 = 0, c2 = 0;
+        for (int& stone : stones) {
+            if (stone % 3 == 0) c0++;
+            else if (stone % 3 == 1) c1++;
+            else c2++;
+        }
+
+        if (c0 % 2 == 0) {
+            return (c1 >= 1 && c2 >= 1) && (c2 >= c1 || c1 >= c2);
+        }
+        else {
+            return abs(c1 - c2) >= 3;
+        }
+    }
+};
+
+int main () {
+    int n; cin >> n;
+    vector<int> stones(n);
+    for (int i = 0; i < n; i++) {
+        cin >> stones[i];
+    }
+    Solution obj;
+    cout << ((obj.stoneGameIX(stones)) ? "true" : "false") << endl;
+    return 0;
+}
