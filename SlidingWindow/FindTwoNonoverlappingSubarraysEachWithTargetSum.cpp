b1b183acedb273e97c6317a0a14b18872b8bcdc6
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2026-09-18 00:08:53 +0530

    Solved LeetCode 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum (Medium)!

---
+++ SlidingWindow/FindTwoNonoverlappingSubarraysEachWithTargetSum.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSumOfLengths(vector<int>& arr, int target) {
+        int n = arr.size();
+        int i = 0, j = 0;
+        int currSum = 0;
+        vector<int> minLenTillIdx(n, INT_MAX);
+        int bestMinLen = INT_MAX;
+        int result = INT_MAX;
+
+        while (j < n) {
+            currSum += arr[j];
+            while (i < j && currSum > target) {
+                currSum -= arr[i];
+                i++;
+            }
+
+            if (currSum == target) {
+                int len = j - i + 1;
+                if (i > 0 && minLenTillIdx[i - 1] != INT_MAX) {
+                    result = min(result, len + minLenTillIdx[i - 1]);
+                }
+                bestMinLen = min(bestMinLen, len);
+            }
+            minLenTillIdx[j] = bestMinLen;
+            j++;
+        }
+        return result == INT_MAX ? -1 : result;
+    }
+};
+
+int main () {
+    int n; cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int target;
+    cin >> target;
+    Solution* obj = new Solution();
+    cout << obj->minSumOfLengths(arr, target) << endl;
+    return 0;
+}
